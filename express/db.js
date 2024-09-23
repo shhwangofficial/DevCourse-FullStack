d34dc2b962ee7f9ba4ffcf5a91e83e47fc99e5b5
shhwang0522 <shhwangofficial@gmail.com>
2024-09-23 16:28:22 +0900

    Change localhost to 127.0.0.1

--- express/db.js
+++ express/db.js
@@ -1,7 +1,7 @@
 import { createConnection } from "mysql2";
 
 const connection = createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   port: 3306, // mariadb 시작시 지정한 port
   user: "root", // mariadb 시작시 지정한 username
   password: "root", // mariadb 시작시 지정한 password
