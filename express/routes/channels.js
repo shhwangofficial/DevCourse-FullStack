6090839ac5f65a50adef687948282f9d2537be28
shhwang0522 <shhwangofficial@gmail.com>
2024-09-24 15:27:53 +0900

    Modify channels API to send SQL queries, Add validation

--- express/routes/channels.js
+++ express/routes/channels.js
@@ -1,17 +1,31 @@
 import express from "express";
 import conn from "../db.js";
+import { body, param, validationResult } from "express-validator";
 const router = express.Router();
 router.use(express.json());
 
 router
   .route("/")
-  .get((req, res) => {
-    // 채널 전체 조회 (소유주)
-    let { userId } = req.body;
-    let sql = `SELECT * FROM channels WHERE user_id = ?`;
-    let values = userId;
-    if (userId) {
+  .get(
+    [body("userId").notEmpty().isInt().withMessage("숫자 입력 필요")],
+    (req, res) => {
+      // 채널 전체 조회 (소유주)
+      const err = validationResult(req);
+
+      if (!err.isEmpty()) {
+        return res.status(400).json(err.array());
+      }
+
+      let { userId } = req.body;
+      let sql = `SELECT * FROM channels WHERE user_id = ?`;
+      let values = userId;
+
       conn.query(sql, values, (err, results) => {
+        if (err) {
+          // 유효성 검사는 ok이지만, 틀린 SQL이어서 에러가 나면 여기로
+          return res.status(400).end();
+        }
+
         if (results.length) {
           res.status(200).json(results);
         } else {
@@ -20,74 +34,101 @@ router
           });
         }
       });
-    } else {
-      res.status(400).end();
     }
-  })
-  .post((req, res) => {
+  )
+  .post(
     // 채널 개별 생성
-    const { name, userId } = req.body;
-    if (name && userId) {
+    [
+      // 배열을 post의 첫 파라미터로 넣어준다.
+      body("userId") // body 메소드는 req.body에서 해당 프로퍼티를 찾는다
+        .notEmpty() // not empty 이어야하고
+        .isInt() // int형이어야한다.
+        .withMessage("userId는 숫자이어야합니다."), // 그렇지 못한 경우, validationResult(req)에 이런 메세지가 들어간 에러json이 들어간다.
+      body("name")
+        .notEmpty()
+        .isString()
+        .withMessage("name은 문자열이어야합니다."),
+    ],
+    (req, res) => {
+      // 두번째 파라미터에 비로소 친숙한 콜백함수
+      const err = validationResult(req);
+
+      if (!err.isEmpty()) {
+        // 위에서 규정한 유효성 검사에 탈락하면 err에는 무언가가 담긴다.
+        return res.status(400).json(err.array()); // 에러 배열 출력
+      }
+
+      const { name, userId } = req.body;
+      // if (name && userId) {  // 더이상 필요없는 if문으로 유효성 검사하기
       let sql = `INSERT INTO channels (name, user_id) VALUES (?, ?)`;
       let values = [name, userId];
 
       conn.query(sql, values, (err, results, fields) => {
+        if (err) {
+          // 유효성 검사는 ok이지만, 틀린 SQL이어서 에러가 나면 여기로
+          return res.status(400).end();
+        }
         res.status(201).json(results);
       });
-    } else {
-      res.status(400).json({
-        message: "입력 값을 확인하세요",
-      });
+      // }
     }
-  });
+  );
 
 router
   .route("/:id")
-  .put((req, res) => {
-    // 채널 개별 수정
-    let { id } = req.params;
-    id = parseInt(id);
-    let channel = db.get(id);
-    let oldTitle = channel.channelTitle;
+  .put(
+    [
+      param("id").notEmpty().withMessage("체널아이디 입력 필요"),
+      body("name").notEmpty().isString().withMessage("채널명 오류"),
+    ],
+    (req, res) => {
+      // 채널 개별 수정
+      const err = validationResult(req);
 
-    if (channel) {
-      const newTitle = req.body.channelTitle;
-      db.set(id, channel);
-      res.status(200).json({
-        message: `${oldTitle}에서 ${newTitle}로 정상적으로 수정 되었습니다.`,
-      });
-    } else {
-      res.status(404).json({
-        message: "채널 정보를 찾을 수 없습니다.",
+      if (!err.isEmpty()) {
+        // 위에서 규정한 유효성 검사에 탈락하면 err에는 무언가가 담긴다.
+        return res.status(400).json(err.array()); // 에러 배열 출력
+      }
+
+      let { id } = req.params;
+      id = parseInt(id);
+      let { name } = req.body;
+      let sql = `UPDATE channels set name = ? WHERE id = ?`;
+      let values = [name, id];
+
+      conn.query(sql, values, (err, results) => {
+        // UPDATE 쿼리의 경우 results는 업데이트된 레코드의 정보를 반환하는 대신 영향을 받은 행(row)의 수를 포함한 객체를 반환.
+        if (err) {
+          // 유효성 검사는 ok이지만, 틀린 SQL이어서 에러가 나면 여기로
+          return res.status(400).end();
+        }
+        if (results.affectedRows) {
+          res.status(200).json(results);
+        } else {
+          res.status(404).json({
+            message: "채널 정보를 찾을 수 없습니다.",
+          });
+        }
       });
     }
-  })
-  .delete((req, res) => {
+  )
+  .delete([param("id").notEmpty().withMessage("id 입력 필요")], (req, res) => {
     // 채널 개별 삭제
-    let { id } = req.params;
-    id = parseInt(id);
-    let channel = db.get(id);
-    if (channel) {
-      db.delete(id);
-      res.status(200).json({
-        message: `${channel.channelTitle}이 정상적으로 삭제되었습니다.`,
-      });
-    } else {
-      res.status(404).json({
-        message: "채널 정보를 찾을 수 없습니다.",
-      });
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+      res.status(400).json(err.array());
     }
-  })
-  .get((req, res) => {
-    // 채널 개별 조회
+
     let { id } = req.params;
     id = parseInt(id);
 
-    let sql = `SELECT * FROM channels WHERE id = ?`;
+    let sql = `DELETE FROM channels where id = ?`;
     let values = [id];
-
     conn.query(sql, values, (err, results) => {
-      if (results.length) {
+      if (err) {
+        return res.status(400).end();
+      }
+      if (results.affectedRows) {
         res.status(200).json(results);
       } else {
         res.status(404).json({
@@ -95,6 +136,38 @@ router
         });
       }
     });
-  });
+  })
+  .get(
+    [param("id").notEmpty().withMessage("체널아이디 입력 필요")],
+    (req, res) => {
+      // 채널 개별 조회
+      const err = validationResult(req);
+
+      if (!err.isEmpty()) {
+        // 위에서 규정한 유효성 검사에 탈락하면 err에는 무언가가 담긴다.
+        return res.status(400).json(err.array()); // 에러 배열 출력
+      }
+
+      let { id } = req.params;
+      id = parseInt(id);
+
+      let sql = `SELECT * FROM channels WHERE id = ?`;
+      let values = [id];
+
+      conn.query(sql, values, (err, results) => {
+        if (err) {
+          // 유효성 검사는 ok이지만, 틀린 SQL이어서 에러가 나면 여기로
+          return res.status(400).end();
+        }
+        if (results.length) {
+          res.status(200).json(results);
+        } else {
+          res.status(404).json({
+            message: "채널 정보를 찾을 수 없습니다.",
+          });
+        }
+      });
+    }
+  );
 
 export default router;
