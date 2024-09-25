0148830df2211e22e04b49800c97537fff0ba32d
shhwang0522 <shhwangofficial@gmail.com>
2024-09-25 13:36:39 +0900

    Functionalize validation process

--- express/routes/channels.js
+++ express/routes/channels.js
@@ -4,10 +4,24 @@ import { body, param, validationResult } from "express-validator";
 const router = express.Router();
 router.use(express.json());
 
+const validation = (req, res, next) => {
+  // next를 파라미터로 준다.
+  const err = validationResult(req);
+
+  if (err.isEmpty()) {
+    next(); // err 비었으면 다음 콜백 실행
+  } else {
+    return res.status(400).json(err.array());
+  }
+};
+
 router
   .route("/")
   .get(
-    [body("userId").notEmpty().isInt().withMessage("숫자 입력 필요")],
+    [
+      body("userId").notEmpty().isInt().withMessage("숫자 입력 필요"),
+      validation,
+    ],
     (req, res) => {
       // 채널 전체 조회 (소유주)
       const err = validationResult(req);
@@ -48,15 +62,11 @@ router
         .notEmpty()
         .isString()
         .withMessage("name은 문자열이어야합니다."),
+      validation,
     ],
     (req, res) => {
       // 두번째 파라미터에 비로소 친숙한 콜백함수
       const err = validationResult(req);
-
-      if (!err.isEmpty()) {
-        // 위에서 규정한 유효성 검사에 탈락하면 err에는 무언가가 담긴다.
-        return res.status(400).json(err.array()); // 에러 배열 출력
-      }
 
       const { name, userId } = req.body;
       // if (name && userId) {  // 더이상 필요없는 if문으로 유효성 검사하기
@@ -80,15 +90,10 @@ router
     [
       param("id").notEmpty().withMessage("체널아이디 입력 필요"),
       body("name").notEmpty().isString().withMessage("채널명 오류"),
+      validation,
     ],
     (req, res) => {
       // 채널 개별 수정
-      const err = validationResult(req);
-
-      if (!err.isEmpty()) {
-        // 위에서 규정한 유효성 검사에 탈락하면 err에는 무언가가 담긴다.
-        return res.status(400).json(err.array()); // 에러 배열 출력
-      }
 
       let { id } = req.params;
       id = parseInt(id);
@@ -112,41 +117,34 @@ router
       });
     }
   )
-  .delete([param("id").notEmpty().withMessage("id 입력 필요")], (req, res) => {
-    // 채널 개별 삭제
-    const err = validationResult(req);
-    if (!err.isEmpty()) {
-      res.status(400).json(err.array());
+  .delete(
+    [param("id").notEmpty().withMessage("id 입력 필요"), validation],
+    (req, res) => {
+      // 채널 개별 삭제
+
+      let { id } = req.params;
+      id = parseInt(id);
+
+      let sql = `DELETE FROM channels where id = ?`;
+      let values = [id];
+      conn.query(sql, values, (err, results) => {
+        if (err) {
+          return res.status(400).end();
+        }
+        if (results.affectedRows) {
+          res.status(200).json(results);
+        } else {
+          res.status(404).json({
+            message: "채널 정보를 찾을 수 없습니다.",
+          });
+        }
+      });
     }
-
-    let { id } = req.params;
-    id = parseInt(id);
-
-    let sql = `DELETE FROM channels where id = ?`;
-    let values = [id];
-    conn.query(sql, values, (err, results) => {
-      if (err) {
-        return res.status(400).end();
-      }
-      if (results.affectedRows) {
-        res.status(200).json(results);
-      } else {
-        res.status(404).json({
-          message: "채널 정보를 찾을 수 없습니다.",
-        });
-      }
-    });
-  })
+  )
   .get(
-    [param("id").notEmpty().withMessage("체널아이디 입력 필요")],
+    [param("id").notEmpty().withMessage("체널아이디 입력 필요"), validation],
     (req, res) => {
       // 채널 개별 조회
-      const err = validationResult(req);
-
-      if (!err.isEmpty()) {
-        // 위에서 규정한 유효성 검사에 탈락하면 err에는 무언가가 담긴다.
-        return res.status(400).json(err.array()); // 에러 배열 출력
-      }
 
       let { id } = req.params;
       id = parseInt(id);
