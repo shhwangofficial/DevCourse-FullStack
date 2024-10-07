bd9f41ede611afca5e2b545aaad5ce779c4cda60
shhwang0522 <shhwangofficial@gmail.com>
2024-10-07 15:01:15 +0900

    Implement new books and categorized books query

--- bookStore/app.js
+++ bookStore/app.js
@@ -5,6 +5,7 @@ import bookRouter from "./routes/books.js";
 import cartRouter from "./routes/carts.js";
 import likeRouter from "./routes/likes.js";
 import orderRouter from "./routes/orders.js";
+import categoryRouter from "./routes/category.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,7 @@ app.use("/books", bookRouter);
 app.use("/carts", cartRouter);
 app.use("/likes", likeRouter);
 app.use("/orders", orderRouter);
+app.use("/category", categoryRouter);
 
 console.log("PORT:", process.env.PORT);
 app.listen(process.env.PORT);
--- bookStore/controller/BookController.js
+++ bookStore/controller/BookController.js
@@ -2,34 +2,36 @@ import conn from "../db.js";
 import { StatusCodes } from "http-status-codes";
 
 const allBooks = (req, res) => {
-  let { category_id } = req.query;
-  if (category_id) {
-    category_id = parseInt(category_id);
-    let sql = `SELECT * FROM books WHERE category_id = ?`;
-    conn.query(sql, category_id, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      if (results.length) return res.status(StatusCodes.OK).json(results);
-      else return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    let sql = `SELECT * FROM books`;
-    conn.query(sql, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
+  let { category_id, news } = req.query;
+
+  let sql = "SELECT * FROM books";
+  let values = [];
+  if (category_id && news) {
+    sql +=
+      " WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    values = [category_id];
+  } else if (category_id) {
+    sql += " WHERE category_id=? ";
+    values = [category_id];
+  } else if (news) {
+    sql +=
+      " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
   }
+
+  conn.query(sql, values, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length) return res.status(StatusCodes.OK).json(results);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
 };
 
 const bookDetail = (req, res) => {
   let { id } = req.params;
   id = parseInt(id);
-  let sql = `SELECT * FROM books WHERE id = ?`;
+  let sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id = ?`;
 
   conn.query(sql, id, (err, results, fields) => {
     if (err) {
