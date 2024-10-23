d0f51982b27d40c62b6150f0f64f1e7e894b172a
shhwang0522 <shhwangofficial@gmail.com>
2024-10-23 15:51:32 +0900

    feat: returns status code 400 if join fails

--- bookStore/controller/UserController.js
+++ bookStore/controller/UserController.js
@@ -22,7 +22,11 @@ const join = (req, res) => {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
     }
-    return res.status(StatusCodes.CREATED).json(results);
+    if (results.affectedRows) {
+      return res.status(StatusCodes.CREATED).json(results);
+    } else {
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
   });
 };
 
