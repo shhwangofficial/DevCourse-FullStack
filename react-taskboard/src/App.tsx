af1ccf35344fdd9fad4402a6ee4fb26d6543887b
shhwang0522 <shhwangofficial@gmail.com>
2024-11-05 15:46:56 +0900

    11/5

--- react-taskboard/src/App.tsx
+++ react-taskboard/src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import { appContainer, board, buttons } from "./App.css";
+import BoardList from "./components/BoardList/BoardList";
 
 function App() {
+  const [activeBoardId, setActiveBoardId] = useState("board-0");
   return (
     <div className={appContainer}>
-      <div className={board}></div>
+      <BoardList
+        activeBoardId={activeBoardId}
+        setActiveBoardId={setActiveBoardId}
+      />
+      <div className={board}>
+        
+      </div>
       <div className={buttons}>
         <button>이 게시판 삭제하기</button>
         <button></button>
--- react-taskboard/src/components/SideForm/SideForm.tsx
+++ react-taskboard/src/components/SideForm/SideForm.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { ChangeEvent, FC, useState } from "react";
+import { FiCheck } from "react-icons/fi";
 
-const SideForm = () => {
+type TSideFormProps = {
+  setIsFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  inputRef: React.RefObject<HTMLInputElement>;
+};
+const SideForm: FC<TSideFormProps> = ({ setIsFormOpen, inputRef }) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+  const handleOnBlur = () => {
+    setIsFormOpen(false);
+  };
   return (
-    <div>SideForm</div>
-  )
-}
+    <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="새로운 게시판 등록하기"
+        value={inputText}
+        onChange={handleChange}
+        onBlur={handleOnBlur}
+      ></input>
+      <FiCheck />
+    </div>
+  );
+};
 
-export default SideForm
+export default SideForm;
